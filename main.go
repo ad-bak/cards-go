db459f7643e0e96d3c0bf76ddb03df43495b37f4
Adnan <adbak.dev@gmail.com>
2023-01-16 00:49:10 +0100

    added deck

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	var card string = "Ace of Spades"
-	fmt.Println(card)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+}
+
+func newCard() string{
+	return "Five of Diamonds"
 }
