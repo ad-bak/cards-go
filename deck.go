44a41ca7ed810859c78c85fd9b02dfc699dbfa7a
Adnan <adbak.dev@gmail.com>
2023-01-16 02:00:12 +0100

    added random

--- deck.go
+++ deck.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string 
@@ -11,7 +15,7 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
 
 	for _, suit := range cardSuits{
 		for _, value := range cardValues{
@@ -33,4 +37,30 @@ func deal(d deck, handSize int) (deck, deck) {
 
 func (d deck) toString() string {
 	return	strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err) 
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle () {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	 
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
